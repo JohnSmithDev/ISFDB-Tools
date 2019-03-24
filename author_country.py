c87974f9ae3bd95b550b96d4fe359b22fe3e3fa9
John Smith (aachen) <aachen@john-smith.me>
2019-03-24 01:07:04 +0000

    Support for an author->country hacks/overrides file.  NB: no such file currently exists in the repo, as I dunno about GDPR/privacy issues (even though my own copy is derived from publicly accessible sources such as Wikipedia and author's personal sites

--- author_country.py
+++ author_country.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-# import logging
-# import re
+import logging
+import re
 import sys
 
 from sqlalchemy.sql import text
@@ -10,6 +10,27 @@ from country_related import get_country
 from common import (get_connection, parse_args, get_filters_and_params_from_args,
                     AmbiguousArgumentsError)
 
+
+COUNTRY_OVERRIDES = 'author_nationality_hacks.txt'
+
+def load_hacks(fn=COUNTRY_OVERRIDES):
+    ret = {}
+    try:
+        with open(fn) as inputstream:
+            for i, raw_line in enumerate(inputstream, 1):
+                line = re.sub('#.*$', '', raw_line).strip()
+                if not line:
+                    continue
+                # print('%d [%s]' % (i, line))
+                try:
+                    author, country = [z.strip() for z in line.split('=')]
+                    ret[author] = country
+                except ValueError as err:
+                    logging.error('Ignoring bad line %d in %s (%s)' %
+                                  (i, fn, err))
+    except FileNotFoundError as err:
+        pass # This is a temp hack whilst the hack file is not in the repo
+    return ret
 
 def get_birthplaces_for_pseudonym(conn, author_id):
     """
@@ -28,8 +49,19 @@ def get_birthplaces_for_pseudonym(conn, author_id):
         return None
 
 
-def get_author_country(conn, filter_args, check_pseudonyms=True):
+def get_author_country(conn, filter_args, check_pseudonyms=True,
+                       overrides=None):
     fltr, params = get_filters_and_params_from_args(filter_args)
+
+    # TODO: support args as a vanilla dict, not just an argparse Namespace
+    if overrides and args.exact_author:
+        # Try to use the override ASAP - if we don't have an exact name, we'll
+        # try again later after we get a name from the database
+        try:
+            return overrides[args.exact_author]
+        except KeyError:
+            pass
+
 
     query = text("""select author_id, author_canonical, author_birthplace
       from authors a
@@ -44,6 +76,8 @@ def get_author_country(conn, filter_args, check_pseudonyms=True):
                                         (len(results), filter_args, results[:5]))
     else:
         rec = results[0]
+        if overrides and rec['author_canonical'] in overrides:
+            return overrides[ rec['author_canonical'] ]
         birthplace = rec['author_birthplace']
         if not birthplace and check_pseudonyms:
             bps = get_birthplaces_for_pseudonym(conn, rec['author_id'])
@@ -58,4 +92,5 @@ if __name__ == '__main__':
                       supported_args='a')
 
     conn = get_connection()
-    print(get_author_country(conn, args))
+    overrides = load_hacks()
+    print(get_author_country(conn, args, overrides=overrides))
