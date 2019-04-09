28acb2821494747237c2232f8b7b615ccf78de45
John Smith (aachen) <aachen@john-smith.me>
2019-04-09 23:53:22 +0100

    Add rainbow colorama lists, although I'm not sure if I'll be using them

--- colorama_wrapper.py
+++ colorama_wrapper.py
@@ -10,3 +10,10 @@ except ImportError:
 
 COLORAMA_RESET = Fore.RESET + Back.RESET + Style.RESET_ALL
 
+# roygbiv-ish
+FG_RAINBOW = [Fore.LIGHTRED_EX, Fore.RED, Fore.LIGHTYELLOW_EX,
+              Fore.LIGHTGREEN_EX, Fore.LIGHTCYAN_EX, Fore.BLUE,
+              Fore.MAGENTA]
+BG_RAINBOW = [Back.LIGHTRED_EX, Back.RED, Back.LIGHTYELLOW_EX,
+              Back.LIGHTGREEN_EX, Back.LIGHTCYAN_EX, Back.BLUE,
+              Back.MAGENTA]
