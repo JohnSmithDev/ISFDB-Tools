14053b324fcfffbea6f7c6c15e134a13e7122e56
John Smith (aachen) <aachen@john-smith.me>
2019-06-24 16:21:47 +0100

    Implement duplicate removal a different way; merge multiple authors for the same book/review

--- magazine_reviews.py
+++ magazine_reviews.py
@@ -20,6 +20,7 @@ Magazines which are somewhat compromised:
 from collections import defaultdict
 from datetime import date
 from enum import Enum
+from functools import reduce
 import logging
 import pdb
 import sys
@@ -30,10 +31,15 @@ from common import (get_connection, create_parser, parse_args,
                     get_filters_and_params_from_args)
 from isfdb_utils import convert_dateish_to_date
 
+
+class DuplicateReviewError(Exception):
+    pass
+
 class RepeatReviewBehaviour(Enum):
     ALLOW = 1,
-    DIFFERENT_ISSUES_ONLY = 2,
-    DISALLOW = 3
+    DIFFERENT_MONTHS_ONLY = 2,
+    DIFFERENT_YEARS_ONLY = 3,
+    DISALLOW = 4
 
 
 
@@ -54,9 +60,11 @@ def normalize_month(dt):
         return None
 
 class ReviewedWork(object):
-    def __init__(self, row):
+    _known_keys = {}
+
+    def __init__(self, row, allow_duplicates=RepeatReviewBehaviour.ALLOW):
         self.title = row['work_title']
-        self.author = row['work_author']
+        self.authors = {row['work_author']}
         self.title_id = row['work_id']
         self.pub_id = row['pub_id']
         self.review_month = normalize_month(convert_dateish_to_date(row['review_month']))
@@ -71,6 +79,32 @@ class ReviewedWork(object):
         else:
             pub_months[row['pub_id']] = self.review_month
 
+        if allow_duplicates != RepeatReviewBehaviour.ALLOW:
+            if allow_duplicates == RepeatReviewBehaviour.DIFFERENT_MONTHS_ONLY:
+                dupe_key = (self.title_id, self.review_month)
+            elif allow_duplicates == RepeatReviewBehaviour.DIFFERENT_YEARS_ONLY:
+                dupe_key = (self.title_id, self.review_year)
+            else: # allow_duplicates == RepeatReviewBehaviour.DISALLOW
+                dupe_key = self.title_id
+            if dupe_key in self._known_keys:
+                # Possibly caused by multiple authors producing multiple rows,
+                # in which case merge them back before aborting this object
+                other = self._known_keys[dupe_key]
+                other.authors.update(self.authors)
+
+                raise DuplicateReviewError('Already have a review for %s (%s)' %
+                                           (self.title, dupe_key))
+            self._known_keys[dupe_key] = self
+
+
+    @property
+    def author(self):
+        return '+'.join(sorted(self.authors))
+
+    @property
+    def review_year(self):
+        return self.review_month.year
+
     def __repr__(self):
         return '"%s" (title_id=%d) by %s reviewed %s' % (self.title,
                                                          self.title_id,
@@ -80,7 +114,7 @@ class ReviewedWork(object):
 
 
 
-def get_reviews(conn, args, repeats=RepeatReviewBehaviour.DIFFERENT_ISSUES_ONLY):
+def get_reviews(conn, args, repeats=RepeatReviewBehaviour.DIFFERENT_MONTHS_ONLY):
     fltr, params = get_filters_and_params_from_args(
         args, column_name_prefixes={'year': 'pub'})
 
@@ -120,28 +154,18 @@ WHERE pub_id IN (
     params['work_ttypes'] = WORK_TTYPES_OF_INTEREST
     params['magazine'] = args.magazine
     results = conn.execute(query, **params).fetchall()
-    reviews = [ReviewedWork(z) for z in results]
-    if repeats != RepeatReviewBehaviour.ALLOW:
-        # As things stand, the newest review is the one that survives - probably
-        # not the best, but will be OK for now (esp. as we default to DIFFERENT_ISSUES_ONLY)
-        reviewed_in_pub = {}
-        deduped_reviews = []
-        for review in reviews:
-            try:
-                extant_reviews = reviewed_in_pub[review.title_id]
 
+    def make_list_excluding_duplicates(accumulator, new_value):
+        if not accumulator:
+            accumulator = []
+        try:
+            accumulator.append(ReviewedWork(new_value,
+                                            allow_duplicates=repeats))
+        except DuplicateReviewError:
+            pass
+        return accumulator
 
-                # logging.debug('%s is a repeat review' % (review.title))
-                if repeats == RepeatReviewBehaviour.DIFFERENT_ISSUES_ONLY and \
-                   review.pub_id not in extant_reviews:
-                    deduped_reviews.append(review)
-                reviewed_in_pub[review.title_id].add(review.pub_id)
-            except KeyError:
-                # logging.debug('%s is a new review' % (review.title))
-                deduped_reviews.append(review)
-                reviewed_in_pub[review.title_id] = set([review.pub_id])
-        reviews = deduped_reviews
-
+    reviews = reduce(make_list_excluding_duplicates, results, None)
     return sorted(reviews, key=lambda z: z.review_month)
 
 
