52fcb3403bda286000ec2d508f63fd4312c256cf
John Smith (aachen) <aachen@john-smith.me>
2019-10-29 18:05:13 +0000

    Tweaked database setup script, primarily to aid in adding tests to the ISFDB site code

--- setup_testing_database.sh
+++ setup_testing_database.sh
@@ -3,21 +3,34 @@
 #
 # * Creating a database named after the dated dump being used (if it doesn't
 #   already exist)
-# * Converting it from MyISAM to InnoDB (so that we can run - and more importantly,
-#   roll back - transactions
-# * Creating readwrite and readonly user accounts.  (readonly is a slight misnomer,
-#   as it can write to the metadata table)
+# * (Optionally) Converting it from MyISAM to InnoDB (so that we can run -
+#   and more importantly, roll back - transactions, which might be useful
+#   for running tests in some circumstances.
+# * Creating readwrite and readonly MySQL user accounts.  (readonly is
+#   a slight misnomer, as it can write to the metadata table)
+# * (Optionally) creating application accounts, using external code from
+#   the ISFDB source code repo
 #
 # Usage:
-#   setup_testing_database backup-MySQL-55-20yy-mm-dd mysql-root-user mysql-root-password
+#   setup_testing_database -d-u mysql-root-user -p mysql-root-password backup-MySQL-55-20yy-mm-dd
 # will create a database called isdfdb-20yy-mm-dd
+#
+# This may need to be run as root (or a *nix user with mysql permissions) in
+# order for stuff like the moving of directory (-d argument) to work.
 
 DATABASE_DIR=""
-while getopts "d:p:u:" arg
+CONVERT_TO_INNODB=0
+CREATE_ACCOUNTS=0
+# ISFDB_REPO_DIR="/proj/3rdparty/isfdb-code-svn"
+ISFDB_REPO_DIR=""
+while getopts "aid:p:r:u:" arg
 do
     case $arg in
+        a) CREATE_ACCOUNTS=1 ;;
         d) DATABASE_DIR="$OPTARG" ;;
+        i) CONVERT_TO_INNODB=1 ;;
         p) MYSQL_ROOT_PASSWORD="$OPTARG" ;;
+        r) ISFDB_REPO_DIR="$OPTARG" ;;
         u) MYSQL_ROOT_USER="$OPTARG" ;;
     esac
 done
@@ -29,15 +42,16 @@ then
     exit 1
 fi
 
-
 DUMP_NAME=$1
 DUMP_BASE=`basename $DUMP_NAME`
 DATE=`echo $DUMP_BASE | sed 's/backup-MySQL-55-//g'`
 DATABASE=`echo "isfdb-${DATE}" | sed 's/\-/_/g'` # Avoids needing to quote (I think)
-
+if [ $CONVERT_TO_INNODB -eq 1 ]
+then
+    DATABASE="${DATABASE}_innodb"
+fi
 
 # echo "$DUMP_NAME $MYSQL_ROOT_USER $MYSQL_ROOT_PASSWORD $DATABASE_DIR"
-
 
 run_mysql_statement() {
     STATEMENT=$1
@@ -53,8 +67,10 @@ run_batch_mysql_statement() {
           -B -e "${STATEMENT}"
 }
 
-echo "Creating database ${DATABASE}"
 
+#####################################################################
+
+echo "Creating database ${DATABASE}"
 
 
 ###
@@ -75,9 +91,7 @@ then
     /bin/rm -f ${DATABASE_LOCATION}
 fi
 
-
 mysqladmin --password=${MYSQL_ROOT_PASSWORD} create $DATABASE
-
 
 # Ugly hack time: I don't want to put the test database on my small root
 # partition.  However, I don't see there's a "proper" way to specify the DB
@@ -97,12 +111,12 @@ fi
 
 
 ###
-### Create the user accounts, and give them basic privs
+### Create the (MySQL/MariaDB) user accounts, and give them basic privs
 ###
 
 HOSTS='localhost %' # Q: Is "%" alone sufficient?
 
-create_user() {
+create_database_user() {
     USER=$1
     for HOST in $HOSTS
     do
@@ -118,8 +132,8 @@ create_user() {
     echo
 }
 
-create_user readonly
-create_user readwrite
+create_database_user readonly
+create_database_user readwrite
 
 for HOST in $HOSTS
 do
@@ -127,21 +141,10 @@ do
     run_mysql_statement "GRANT ALL ON ${DATABASE}.* TO 'readwrite'@'${HOST}';"
 done
 
+
 ###
 ### Run the dump restore script
 ###
-
-# Weird error message, not sure what this is about (didn't appear until script
-# had been running for several minutes:
-# Populating database from dump script /mnt/data2019/_isfdb_/cygdrive.20190928/c/ISFDB/Backups/backup-MySQL-55-2019-09-28 ...
-# ./setup_testing_database.sh: line 98: --password=readwrite: command not found
-
-# Ran again (after minor tweaks), and another WTF error:
-# Populating database from dump script /mnt/data2019/_isfdb_/cygdrive.20190928/c/ISFDB/Backups/backup-MySQL-55-2019-09-28 ...
-# ./setup_testing_database.sh: line 106: [: -ne: unary operator expected
-# Although I think that's a typo on my part, on closer inspection
-
-
 
 # TODO (maybe): Do a replace on the script of "MyISAM" to "InnoDB", so that
 # we don't need the following InnoDB conversion step
@@ -154,28 +157,28 @@ if [ $RETVAL -ne 0 ]
 then
     echo "WARNING: database population had non-zero exit status ($RETVAL)"
 fi
-
-
 echo
 echo
 
 ###
-### Convert to InnoDB
+### (Optionally) convert to InnoDB
 ###
-
-# See https://dba.stackexchange.com/questions/35073/modify-all-tables-in-a-database-with-a-single-command/35089
-# for a (probably) better way of doing all the tables
-run_batch_mysql_statement "SHOW TABLES;" | while read TABLE
-do
-    if [[ $TABLE =~ Tables_in ]]
-    then
-        # Skip header
-        echo
-    else
-        # echo "Table ---> $TABLE"
-        run_mysql_statement "ALTER TABLE $TABLE engine=innodb;"
-    fi
-done
+if [ $CONVERT_TO_INNODB -eq 1]
+then
+    # See https://dba.stackexchange.com/questions/35073/modify-all-tables-in-a-database-with-a-single-command/35089
+    # for a (probably) better way of doing all the tables
+    run_batch_mysql_statement "SHOW TABLES;" | while read TABLE
+    do
+        if [[ $TABLE =~ Tables_in ]]
+        then
+            # Skip header
+            echo
+        else
+            # echo "Table ---> $TABLE"
+            run_mysql_statement "ALTER TABLE $TABLE engine=innodb;"
+        fi
+    done
+fi
 
 
 ###
@@ -186,3 +189,78 @@ do
     # I think UPDATE is enough?
     run_mysql_statement "GRANT UPDATE ON ${DATABASE}.metadata TO 'readonly'@'${HOST}';"
 done
+
+
+
+
+
+###
+### Create the application users
+### See scripts/create_user.py in the ISFDB repo for the origin of this,
+### especially the hashed password.
+### Note: This relies on a refactored version of create_user.py which doesn't
+###       currently exist in the ISFDB repo.
+###
+
+create_application_user() {
+    USERNAME=$1
+    PRIVILEGE=$2
+
+    TEMP_DIR=/tmp/setup_testing_database.$$
+    mkdir -p ${TEMP_DIR}
+
+
+    cat <<EOF1 > ${TEMP_DIR}/localdefs.py
+# This was automatically generated by setup_testing_database.sh
+
+HTMLLOC = "localhost"
+HTFAKE = "/localhost/cgi-bin"
+DBASEHOST = "localhost"
+HTMLHOST = "localhost"
+COOKIEHOST = "localhost"
+WIKILOC = "localhost/wiki"
+
+
+# For most tests, it's the next three values that are the only ones that will
+# be used
+USERNAME = "readwrite"
+PASSWORD = "readwrite"
+DBASE = "$DATABASE"
+
+UNICODE = "iso-8859-1"
+DO_ANALYTICS = 0
+
+EOF1
+
+
+    # Copying the script is a hack to force it to use our localdefs.py
+    # rather than the one in the repo directory - it seems that the directory
+    # of the called script will always be at the front of sys.path regardless
+    # of anything we set PYTHONPATH to.
+    cp -p ${ISFDB_REPO_DIR}/scripts/create_user.py ${TEMP_DIR}
+    python2 ${TEMP_DIR}/create_user.py ${USERNAME} ${USERNAME} ${PRIVILEGE}
+
+    # Earlier version that required a refactored version of create_user.py - works,
+    # but doesn't need to be this complicated
+
+#    PYTHONPATH=${TEMP_DIR}:/proj/3rdparty/isfdb-code-svn/scripts python2 - <<EOF2
+#from create_user import update_database, setup_database
+#
+#db = setup_database("localhost", "$MYSQL_ROOT_USER", "$MYSQL_ROOT_PASSWORD", "$DATABASE")
+#print("$USERNAME","$PRIVILEGE")
+#update_database(db, "$USERNAME", "$USERNAME", $PRIVILEGE)
+#db.commit()
+#db.close()
+#EOF2
+
+
+    /bin/rm -rf ${TEMP_DIR}
+}
+
+
+if [ $CREATE_ACCOUNTS -eq 1 ]
+then
+    create_application_user TestModerator 1
+    create_application_user TestEditor 0
+fi
+
