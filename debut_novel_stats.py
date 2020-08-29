8fc1ba1f1cc044d7246ec96e103e2d10cc4868ca
John Smith (aachen) <aachen@john-smith.me>
2020-08-29 19:46:45 +0100

    Optionally show more detail about all the titles published

--- debut_novel_stats.py
+++ debut_novel_stats.py
@@ -15,7 +15,8 @@ import sys
 from sqlalchemy.sql import text
 
 
-from common import get_connection, parse_args, get_filters_and_params_from_args
+from common import (get_connection, parse_args, get_filters_and_params_from_args,
+                    create_parser)
 # from isfdb_utils import convert_dateish_to_date, pretty_list
 
 from publisher_books import get_publisher_books
@@ -30,31 +31,53 @@ bibliographies = {}
 
 
 # I'm sure I have tuples for this already, although maybe in another repo?
-AuthorAndTitleStuff = namedtuple('AuthorAndTitleStuff', 'author_id, author, title_id, title')
+# ... although I've now added book, so this isn't the same as the other
+# namedtuples
+AuthorAndTitleStuff = namedtuple('AuthorAndTitleStuff', 'author_id, author, '
+                                 'title_id, title, book')
+
+
+def collate_title_ids(bk):
+    bk_ids = set()
+    # Yay for inconsistent attribute names :-(
+    for a in ['title_id', 'parent_id', 'title_parent']:
+        try:
+            x = getattr(bk, a)
+            if x:
+                bk_ids.add(x)
+        except AttributeError:
+            pass
+    return bk_ids
+
+def is_same_book(bk1, bk2):
+    bk1_ids = collate_title_ids(bk1)
+    bk2_ids = collate_title_ids(bk2)
+    return bk1_ids.intersection(bk2_ids)
+
 
 class DebutStats(object):
     def __init__(self, books, do_prefilter=True):
 
         if do_prefilter:
             novels = [z for z in books if z.publication_type.lower() == 'novel']
-            new_novels = [z for z in novels
+            self.books = [z for z in novels
                           if z.best_copyright_date.year == z.first_publication.year]
         else:
-            new_novels = books
-        self._process(new_novels)
+            self.books = books
+        self._process()
 
 
-    def _process(self, books):
-        self.all_books = []
+    def _process(self):
+        self.all_details = []
         # Dict mapping author name to book details; this is to avoid counting
         # an author twice
         self.debut_details = {}
 
-        for i, bk in enumerate(books, 1):
+        for i, bk in enumerate(self.books, 1):
             for (author_id, author_name) in sorted(bk.author_id_to_name.items()):
                 details = AuthorAndTitleStuff(author_id, author_name,
-                                              bk.title_id, bk.title)
-                self.all_books.append(details)
+                                              bk.title_id, bk.title, bk)
+                self.all_details.append(details)
                 author_ids = [author_id]
                 other_author_stuff = get_real_author_id_and_name(conn, author_id)
                 if other_author_stuff:
@@ -79,25 +102,21 @@ class DebutStats(object):
                 # Check the title IDs and the title parent IDs to be (hopefully)
                 # sure of finding a match
                 debut_novel = bib[0]
-                debut_ids = set([debut_novel.title_id])
-                if debut_novel.parent_id:
-                    debut_ids.add(debut_novel.parent_id)
-                bk_ids = set([bk.title_id])
-                if bk.title_parent:
-                    bk_ids.add(bk.title_parent)
-                if bk_ids.intersection(debut_ids):
+                if is_same_book(debut_novel, bk):
                     if author_name not in self.debut_details:
                         self.debut_details[author_name] = details
                     # print('debut_ids=%s; bk_ids=%s' % (debut_ids, bk_ids))
                     # print(bk.copyright_date, bk.best_copyright_date)
 
-        for i, bk in enumerate(self.all_books, 1):
+        for i, bk in enumerate(self.all_details, 1):
             # print(i,bk)
             pass
 
     @property
     def book_author_count(self):
-        return len(self.all_books)
+        # Use len(self.books) if you want to know the number of books, regardless
+        # of the number of authors
+        return len(self.all_details)
 
     @property
     def debut_authors(self):
@@ -107,6 +126,50 @@ class DebutStats(object):
     def debut_count(self):
         return len(self.debut_authors)
 
+    @property
+    def nth_book_details(self):
+        """
+        Note that the nth book details returned start at 1, whereas other
+        code in this module uses 0.  (The other code doesn't display any number
+        to the user, whereas the expectation is that these values will be shown
+        or used in some other calculation such as average nth book.
+        """
+        ret = []
+        for bk_stuff in self.all_details:
+            bib = bibliographies[bk_stuff.author_id]
+            for i, bib_book in enumerate(bib, 1):
+                if is_same_book(bk_stuff.book, bib_book):
+                    ret.append((i, bk_stuff))
+                    break
+            else:
+                logging.warning(f'Failed to find {bk_stuff.book.title} in bibliography '
+                                f'for {bk_stuff.author} ({bk_stuff.author_id})')
+        return ret
+
+    @property
+    def average_nth_book(self):
+        nth_items = [z[0] for z in self.nth_book_details]
+        weighted_nth_items = [weighted_nth_book(z[0], z[1].book) for z in self.nth_book_details]
+        sorted_items = sorted(nth_items)
+        sorted_weighted_items = sorted(weighted_nth_items)
+        median_idx = int(len(nth_items) / 2)
+        if nth_items:
+            return (sum(nth_items) / len(nth_items) , sorted_items[median_idx],
+                    sum(weighted_nth_items) / len(nth_items) ,
+                    sorted_weighted_items[median_idx])
+        else:
+            return 0, 0, 0, 0
+
+    def output_detail(self, output_function=print):
+        details = self.nth_book_details
+        max_nth = max([z[0] for z in details])
+        # TODO: work out optimal width
+        for nth, bk in sorted(details):
+            weighted_nth = weighted_nth_book(nth, bk.book)
+            nth_string = f'#{nth} [weighted={weighted_nth}]'
+            output_function('Book %3s: "%s" by %s' % (nth_string,
+                                                      bk.title, bk.author))
+
     def __repr__(self):
         return '%2d of %3d (%2d%%) new novels/authors were debuts : %s' % \
             (self.debut_count, self.book_author_count,
@@ -114,6 +177,12 @@ class DebutStats(object):
              ', '.join(sorted(self.debut_authors))
             )
 
+def weighted_nth_book(nth, bk):
+    num_authors = len(bk.authors)
+    if num_authors == 0: # Assume anonymous, unknown, etc
+        return 0
+    else:
+        return max(1, int(nth / len(bk.authors)))
 
 def split_books_by_year(books):
     """
@@ -129,19 +198,28 @@ def split_books_by_year(books):
 def debut_report(conn, args, output_function=print):
 
     results = get_publisher_books(conn, args,
-                                  countries=[z.upper() for z in args.countries])
+                                  countries=[z.upper() for z in args.countries],
+                                  book_types=['NOVEL'])
 
     yearly_results = split_books_by_year(results)
     for year, books in yearly_results:
         ds = DebutStats(books, do_prefilter=True)
 
         if ds.book_author_count:
-            output_function('%s. %s' % (year, ds))
+            mean, weighted_mean, median, weighted_median= ds.average_nth_book
+            output_function('%s. %s (mean %dth/%dth book, median %dth/%dth book)' %
+                            (year, ds, mean, weighted_mean, median, weighted_median))
+            if args.show_detail:
+                ds.output_detail(output_function)
+
 
 if __name__ == '__main__':
-    args = parse_args(sys.argv[1:],
-                      description='Report on debut novels published by a publisher',
-                      supported_args='kpy')
+    parser = create_parser(description='Report on debut novels published by a publisher',
+                           supported_args='kpy')
+    parser.add_argument('-d', dest='show_detail', action='store_true',
+                        help='Enable detailed output')
+    args = parse_args(sys.argv[1:], parser=parser)
+
 
     conn = get_connection()
     debut_report(conn, args)
