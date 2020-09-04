d7a7c6d0219b5a7c7a2250138f772e5c4309fc71
John Smith (aachen) <aachen@john-smith.me>
2020-09-04 20:37:31 +0100

    Pass database connection through to object (rather than relying on global variable); prettier nth display

--- debut_novel_stats.py
+++ debut_novel_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Report on which novels a publisher published were debuts.
+Report on which novels a publisher published were debuts, or an author's
+2nd, 3rd, nth, etc novel.
 
 """
 
@@ -13,6 +14,8 @@ import pdb
 import sys
 
 from sqlalchemy.sql import text
+
+
 
 
 from common import (get_connection, parse_args, get_filters_and_params_from_args,
@@ -36,6 +39,8 @@ bibliographies = {}
 AuthorAndTitleStuff = namedtuple('AuthorAndTitleStuff', 'author_id, author, '
                                  'title_id, title, book')
 
+NthAverages = namedtuple('NthAverages', 'mean_nth, weighted_mean_nth, ' + \
+                         'median_nth, weighted_median_nth')
 
 def collate_title_ids(bk):
     bk_ids = set()
@@ -56,7 +61,8 @@ def is_same_book(bk1, bk2):
 
 
 class DebutStats(object):
-    def __init__(self, books, do_prefilter=True):
+    def __init__(self, books, conn, do_prefilter=True):
+        self.conn = conn
 
         if do_prefilter:
             novels = [z for z in books if z.publication_type.lower() == 'novel']
@@ -79,13 +85,13 @@ class DebutStats(object):
                                               bk.title_id, bk.title, bk)
                 self.all_details.append(details)
                 author_ids = [author_id]
-                other_author_stuff = get_real_author_id_and_name(conn, author_id)
+                other_author_stuff = get_real_author_id_and_name(self.conn, author_id)
                 if other_author_stuff:
                     author_ids.extend([z.id for z in other_author_stuff])
                 try:
                     bib = bibliographies[author_id]
                 except KeyError:
-                    bib = get_bibliography(get_connection(),
+                    bib = get_bibliography(self.conn,
                                            author_ids,
                                            author_name)
                     for aid in author_ids:
@@ -148,27 +154,41 @@ class DebutStats(object):
 
     @property
     def average_nth_book(self):
+        """
+        Returns a (named)tuple of
+        * mean nth book
+        * weighted mean nth book
+        * median nth book
+        * weighted median nth book
+        The weighting is to try to address the bloating that books by multiple
+        authors can cause, by dividing the nth value by the number of authors.
+        (This only looks at the book in question; it doesn't care about any
+        historical collaborations.)
+        """
         nth_items = [z[0] for z in self.nth_book_details]
         weighted_nth_items = [weighted_nth_book(z[0], z[1].book) for z in self.nth_book_details]
         sorted_items = sorted(nth_items)
         sorted_weighted_items = sorted(weighted_nth_items)
         median_idx = int(len(nth_items) / 2)
         if nth_items:
-            return (sum(nth_items) / len(nth_items) , sorted_items[median_idx],
-                    sum(weighted_nth_items) / len(nth_items) ,
-                    sorted_weighted_items[median_idx])
+            return NthAverages(sum(nth_items) / len(nth_items) ,
+                               sorted_items[median_idx],
+                               sum(weighted_nth_items) / len(nth_items) ,
+                               sorted_weighted_items[median_idx])
         else:
             return 0, 0, 0, 0
 
     def output_detail(self, output_function=print):
         details = self.nth_book_details
         max_nth = max([z[0] for z in details])
-        # TODO: work out optimal width
         for nth, bk in sorted(details):
             weighted_nth = weighted_nth_book(nth, bk.book)
-            nth_string = f'#{nth} [weighted={weighted_nth}]'
-            output_function('Book %3s: "%s" by %s' % (nth_string,
-                                                      bk.title, bk.author))
+            pretty_nth = pretty_ordinal(nth)
+            nth_string = f'{pretty_nth} [weighted={weighted_nth}]'
+            output_function('%25s\'s %3s book: "%s"' % (bk.author,
+                                                            nth_string,
+                                                            bk.title))
+
 
     def __repr__(self):
         return '%2d of %3d (%2d%%) new novels/authors were debuts : %s' % \
@@ -176,6 +196,22 @@ class DebutStats(object):
              100 * self.debut_count / self.book_author_count,
              ', '.join(sorted(self.debut_authors))
             )
+
+def pretty_ordinal(n):
+    """Return '1st', '2nd', '3rd' etc for a given integer"""
+    pos_n = abs(n)
+    if (pos_n % 100) in (11, 12, 13):
+        return f'{n}th'
+    elif (pos_n % 10) == 1:
+        return f'{n}st'
+    elif (pos_n % 10) == 2:
+        return f'{n}nd'
+    elif (pos_n % 10) == 3:
+        return f'{n}rd'
+    else:
+        return f'{n}th'
+
+
 
 def weighted_nth_book(nth, bk):
     num_authors = len(bk.authors)
@@ -196,21 +232,32 @@ def split_books_by_year(books):
     return sorted(year_to_books.items())
 
 def debut_report(conn, args, output_function=print):
+    """
+    Besides outputting a textual report, returns a sorted list of
+    (year, DebutStats1.  Only years that had books published will be in the
+    returned value, but this may include years where there were no debut novels.)
+    """
 
+    ret = []
     results = get_publisher_books(conn, args,
                                   countries=[z.upper() for z in args.countries],
                                   book_types=['NOVEL'])
 
     yearly_results = split_books_by_year(results)
-    for year, books in yearly_results:
-        ds = DebutStats(books, do_prefilter=True)
+    for year, books in sorted(yearly_results):
+        ds = DebutStats(books, conn, do_prefilter=True)
 
         if ds.book_author_count:
             mean, weighted_mean, median, weighted_median= ds.average_nth_book
             output_function('%s. %s (mean %dth/%dth book, median %dth/%dth book)' %
                             (year, ds, mean, weighted_mean, median, weighted_median))
-            if args.show_detail:
-                ds.output_detail(output_function)
+            try:
+                if args.show_detail:
+                    ds.output_detail(output_function)
+            except AttributeError:
+                pass
+        ret.append((year, ds))
+    return ret
 
 
 if __name__ == '__main__':
