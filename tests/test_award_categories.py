80ab12946aea8ae81623ed3189aecbf484870dfc
John Smith (kyiv) <kyiv@john-smith.me>
2023-05-16 00:13:56 +0100

    It's a good idea to update tests in sync with functionality changes :-(

--- tests/test_award_categories.py
+++ tests/test_award_categories.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from ..award_categories import render_year_ranges
+from ..award_categories import render_year_ranges, convert_list_string_to_unique_years
 
 class TestRenderYearRanges(unittest.TestCase):
 
@@ -10,46 +10,40 @@ class TestRenderYearRanges(unittest.TestCase):
         self.assertEqual('', render_year_ranges(''))
 
     def test_single_year(self):
-        self.assertEqual('2001', render_year_ranges('2001'))
+        self.assertEqual('2001', render_year_ranges([2001]))
 
     def test_single_yyyymmdd(self):
-        self.assertEqual('2001', render_year_ranges('2001-00-00'))
+        processed = convert_list_string_to_unique_years('2001-00-00')
+        self.assertEqual('2001', render_year_ranges(processed))
 
     def test_contiguous_years(self):
-        self.assertEqual('2001-2004', render_year_ranges('2001,2002,2003,2004'))
+        self.assertEqual('2001-2004', render_year_ranges([2001,2002,2003,2004]))
 
     def test_contiguous_yyyymmdds(self):
+        processed = convert_list_string_to_unique_years('2001-00-00,2002-00-00,2003-00-00,2004-00-00')
         self.assertEqual('2001-2004',
-                         render_year_ranges('2001-00-00,2002-00-00,2003-00-00,2004-00-00'))
+                         render_year_ranges(processed))
 
     def test_separate_years(self):
-        self.assertEqual('2001, 2004, 2010', render_year_ranges('2001,2004,2010'))
+        self.assertEqual('2001, 2004, 2010', render_year_ranges([2001,2004,2010]))
 
     def test_contiguous_yyyymmdds(self):
+        processed = convert_list_string_to_unique_years('2001-00-00,2004-00-00,2010-00-00')
         self.assertEqual('2001, 2004, 2010',
-                         render_year_ranges('2001-00-00,2004-00-00,2010-00-00'))
+                         render_year_ranges(processed))
 
     def test_mixed_years(self):
         self.assertEqual('2001-2004, 2010, 2012-2015',
-                         render_year_ranges('2001,2002,2003,2004,2010,2012,2013,2014,2015'))
+                         render_year_ranges([2001,2002,2003,2004,2010,2012,2013,2014,2015]))
 
     def test_contiguous_yyyymmdds(self):
-        self.assertEqual('2001-2004, 2010, 2012-2015',
-                         render_year_ranges('2001-00-00,2002-00-00,2003-00-00,2004-00-00,'
+        processed = convert_list_string_to_unique_years('2001-00-00,2002-00-00,2003-00-00,2004-00-00,'
                                             '2010-00-00,'
-                                            '2012-00-00,2013-00-00,2014-00-00,2015-00-00'))
-
-    def test_mixed_years_unordered(self):
+                                            '2012-00-00,2013-00-00,2014-00-00,2015-00-00')
         self.assertEqual('2001-2004, 2010, 2012-2015',
-                         render_year_ranges('2015,2002,2010,2004,2003,2014,2013,2012,2001'))
-
-    def test_mixed_years_unordered_and_dupes(self):
-        self.assertEqual('2001-2004, 2010, 2012-2015',
-                         render_year_ranges('2015,2002,2010,2004,2003,2014,2013,2012,2001,'
-                                            '2002,2014'))
+                         render_year_ranges(processed))
 
     def test_mixed_years_character_overrides(self):
         self.assertEqual('2001 to 2004; 2010; 2012 to 2015',
-                         render_year_ranges('2015,2002,2010,2004,2003,2014,2013,2012,2001,'
-                                            '2002,2014',
+                         render_year_ranges([2001,2002,2003,2004,2010,2012,2013,2014,2015],
                                             range_link=' to ', range_separator='; '))
